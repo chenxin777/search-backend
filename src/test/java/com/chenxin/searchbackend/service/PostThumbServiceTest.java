74cf17ac937faf73ffeca6a53b66244c0a0772ce
chenxin777 <chenxinfang77@gmail.com>
2024-05-25 11:40:35 +0800

    聚合搜索平台后端初始化

---
+++ src/test/java/com/chenxin/searchbackend/service/PostThumbServiceTest.java
@@ -0,0 +1,34 @@
+package com.chenxin.searchbackend.service;
+
+import com.chenxin.searchbackend.model.entity.User;
+import javax.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 帖子点赞服务测试
+ *
+ * @author <a href="https://github.com/lichenxin">程序员鱼皮</a>
+ * @from <a href="https://chenxin.icu">编程导航知识星球</a>
+ */
+@SpringBootTest
+class PostThumbServiceTest {
+
+    @Resource
+    private PostThumbService postThumbService;
+
+    private static final User loginUser = new User();
+
+    @BeforeAll
+    static void setUp() {
+        loginUser.setId(1L);
+    }
+
+    @Test
+    void doPostThumb() {
+        int i = postThumbService.doPostThumb(1L, loginUser);
+        Assertions.assertTrue(i >= 0);
+    }
+}
